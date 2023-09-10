8f2e683357c8e9ad38fda1a8eb21f82796f1661e
Johannes <johannessandjaja@proton.me>
2023-09-10 14:28:16 +0700

    can move and save, done

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-constant evaluation for home directory?
-duplicate key mapping?
+Trouble<br>
+[Command](https://doc.rust-lang.org/std/process/struct.Command.html)
+
+This approach doesn't change the working directory of your current shell session directly but rather spawns a new shell process that executes the cd command. Changing the working directory of the current shell session from a child process (your Rust program) is not possible because child processes run in isolated environments.
+<br>
+Workaround = direct bash
+
+# Config
+
+1. Copy the bash function to your .zshrc or .bashrc
+2. Customize the alias according to your preferences for the command.
+3. cargo build --release
+4. Add releases to $PATH
+
+# How to use
+
+```
+p save {{directory alias name}} saving the config to `$HOME/.pindah`
+p move {{asd}} move to the alias directory if exist
+```
--- src/main.rs
+++ src/main.rs
@@ -9,36 +9,37 @@
 
 */
 use clap::{Parser, ValueEnum};
-use std::env;
-use std::fs::{File, OpenOptions};
+use std::fs::{read_to_string, File, OpenOptions};
 use std::io::Write;
 use std::path::{Path, PathBuf};
+use std::{env, process};
 #[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
-enum Command {
+enum Instruction {
     Save,
     Move,
 }
 #[derive(Parser)]
 struct Pindah {
-    /// available command is save or move
+    /// available Instruction is save or move
     #[arg(value_enum)]
-    command: Command,
+    instruction: Instruction,
     attribute: String,
 }
 
-fn main() {
+fn main() -> anyhow::Result<()> {
     let home = find_home_directory();
     let config_file = generate_config(&home);
     let pindah = Pindah::parse();
-    match pindah.command {
-        Command::Save => save(&pindah.attribute, &config_file),
-        Command::Move => move_to(&pindah.attribute),
-    }
+    match pindah.instruction {
+        Instruction::Save => save(&pindah.attribute, &config_file),
+        Instruction::Move => move_to(&pindah.attribute, &home),
+    };
+    Ok(())
 }
 
-fn save(directory_name: &str, mut config: &File) {
+fn save(directory_alias: &str, mut config: &File) {
     let current_dir = env::current_dir().unwrap();
-    let configuration = format!("{}={}", directory_name, current_dir.to_str().unwrap());
+    let configuration = format!("{}={}", directory_alias, current_dir.to_str().unwrap());
     match writeln!(config, "{}", configuration) {
         Ok(_) => {
             println!("Success saving {}", configuration);
@@ -49,8 +50,16 @@ fn save(directory_name: &str, mut config: &File) {
     }
 }
 
-fn move_to(_directory_name: &str) {
-    env::set_current_dir("/").unwrap();
+fn move_to(directory_name_alias: &str, config: &PathBuf) {
+    let data = read_to_string(config).unwrap();
+    for line in data.split('\n') {
+        let parts: Vec<&str> = line.splitn(2, '=').collect();
+        if directory_name_alias == parts[0] {
+            println!("{}", parts[1]);
+            process::exit(1);
+        }
+    }
+    process::exit(99);
 }
 
 fn generate_config(config_path: &PathBuf) -> File {
